599837809bedc85b04a0da4e44ddb3df0bf4cc40
Guerrero Samaniego Cynthia <guerrerocynthia460@gmail.com>
2023-04-28 01:37:29 -0300

    punto 3e

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo");
+    int num = 10;
+    int * puntero;
+    puntero = &num;
+
+    printf("Hola mundo \n");
+    printf("Contenido del puntero: %d \n", *puntero);
+    printf("La direccion de memoria almacenada por el puntero: %p \n", puntero);
+    printf("La direccion de memoria de la variable: %p \n", &num);
+    printf("La direccion de memoria del puntero: %p \n", &puntero);
+    printf("el tamanio de memoria utilizado por esa variable usando la funcion sizeof(): %d", sizeof(num));
 
     return 0;
 }
